74828d94bff303810c0d349aac844d39fa916bc4
iarba <dociarba@gmail.com>
2017-06-02 00:43:47 +0100

    Correctly implemented adapter, 100% tested

--- database/adapter.sh
+++ database/adapter.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-function db_adapter() {
-
-while(true)
-do
-  array=(`nc -l 7777 < fifo1 | head -n 1 | sed 's/[^0-9_ ]//g' | awk '{print $1;}' | tr '_' ' ' | awk '{print "COPY (SELECT * FROM agents.grid WHERE x>="$1" AND x<="$3" AND y>="$2" AND y<="$4") TO STDOUT;";}' | psql`)
-
+function gen_page() {
   cp index.html to_send.html
-
+  array=(`echo "COPY (SELECT * FROM agents.grid) TO STDOUT;" | psql`)
   for (( i=0; i<=$(( ${#array[@]} -1 )); i+=3 ))
   do
     pos=#$(( ${array[$i]} * 60 + ${array[$((i + 1))]} ))
@@ -23,9 +18,14 @@ do
     fi
     eval "sed -i -e 's/\"$pos\"/\"$color\"/g' to_send.html"
   done
+}
 
-  cat to_send | sed -e '1i HTTP/1.1 200 OK\n' > fifo1
-  rm to_send.html
+function db_adapter() {
+
+while(true)
+do
+  gen_page
+  cat to_send.html | sed -e '1i HTTP/1.1 200 OK\n' | nc -l -p 7777
 done
 }
 
