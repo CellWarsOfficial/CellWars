fa2cb5dc5c897878b27e0c745d95b34d46e1a442
iarba <dociarba@gmail.com>
2017-06-01 23:43:29 +0100

    Modified adapter to create html files instead of returning raw data.

--- database/adapter.sh
+++ database/adapter.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
 function db_adapter() {
-  while(true) do nc -l -p 7777 <fifo1 | head -n 1 | sed 's/[^0-9_ ]//g' | awk '{print $1;}' | tr '_' ' ' | awk '{print "COPY (SELECT * FROM agents.grid WHERE x>="$1" AND x<="$3" AND y>="$2" AND y<="$4") TO STDOUT WITH CSV;";}' | psql | sed -e '1i HTTP/1.1 200 OK\n' >fifo1; done
+
+while(true)
+do
+  array=(`nc -l 7777 < fifo1 | head -n 1 | sed 's/[^0-9_ ]//g' | awk '{print $1;}' | tr '_' ' ' | awk '{print "COPY (SELECT * FROM agents.grid WHERE x>="$1" AND x<="$3" AND y>="$2" AND y<="$4") TO STDOUT;";}' | psql`)
+
+  cp index.html to_send.html
+
+  for (( i=0; i<=$(( ${#array[@]} -1 )); i+=3 ))
+  do
+    pos=#$(( ${array[$i]} * 60 + ${array[$((i + 1))]} ))
+    typ=$(( array[$((i + 2))] % 3 ))
+    color=redcell
+    if [[ $typ -eq 1 ]]
+    then
+      color=bluecell
+    fi
+    if [[ $typ -eq 2 ]]
+    then
+      color=redcell
+    fi
+    eval "sed -i -e 's/\"$pos\"/\"$color\"/g' to_send.html"
+  done
+
+  cat to_send | sed -e '1i HTTP/1.1 200 OK\n' > fifo1
+  rm to_send.html
+done
 }
 
 . ~/.pgdb-g1627123_u-g1627123_u
