5816c1c96f63945a72b7377deb2591e61f5c85f7
Paul Crestez <paul.crestez15@imperial.ac.uk>
2017-06-02 06:33:52 +0100

    Updated sv_adapter.sh to no longer parse input.
    It was doing it poorly anyway.

--- database/sv_adapter.sh
+++ database/sv_adapter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function db_sv_adapter() {
-  while(true) do nc -l -p 7778 <fifo2 | awk '{print "COPY ("$0") TO STDOUT WITH CSV;";}' | psql >fifo2; done
+  while(true) do nc -l -p 7778 <fifo2 | psql >fifo2; done
 }
 
 . ~/.pgdb-g1627123_u-g1627123_u
