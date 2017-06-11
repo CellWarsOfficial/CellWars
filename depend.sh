72f95cb356df351c8d12d171ae9410dfe138e62a
iarba <dociarba@gmail.com>
2017-06-11 23:27:24 +0100

    Started reacting with react.
    Modified makefile to accound for react.
    Created file to install dependencies.
    Updated CI to install dependencies.

---
+++ depend.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "installing NPM and nodeJS, required for reactJS."
+apt-get install nodejs 
+apt-get install npm
+ln -s /usr/bin/nodejs /urs/bin/node
+echo "updating nodeJS to latest version using n."
+npm install -g n
+n stable
+echo "updating npm to latest version."
+npm update
+echo "downloading reactJS."
+npm install -g react
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 STDTAR := all check clean
 SPCTAR := install uninstall
-SUBDIR := database game_server
+SUBDIR := client database game_server
 ASIOD = asio-1.10.6
 ASIOLOC = https://sourceforge.net/projects/asio/files/asio/1.10.6%20%28Stable%29/asio-1.10.6.tar.bz2
 RM = rm -rf
