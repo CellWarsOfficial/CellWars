6cb0b2107f7b2791fe06885c8e5d76a1c6339746
iarba <dociarba@gmail.com>
2017-06-13 10:23:37 +0300

    fixed typo and optimised

--- depend.sh
+++ depend.sh
@@ -5,11 +5,11 @@ apt-get --assume-yes update
 echo "installing NPM and nodeJS, required for reactJS."
 apt-get --assume-yes install nodejs 
 apt-get --assume-yes install npm
-ln -s /usr/bin/nodejs /urs/bin/node
+ln -s /usr/bin/nodejs /usr/bin/node
 echo "updating nodeJS to latest version using n."
 cd react/my-app/
-npm install $1 n
-npm install $1 npm@latest
+npm install -g npm@latest
+npm install -g n
 n stable
 echo "updating npm to latest version."
 npm update
