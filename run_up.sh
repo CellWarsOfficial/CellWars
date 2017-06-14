3e46698ee9d3d6dead9869a0d14336b5d4ea7e45
iarba <dociarba@gmail.com>
2017-06-14 13:25:38 +0100

    another update to run_up to be quiet

--- run_up.sh
+++ run_up.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-make uninstall
 rm -rf .tmpoutput
 touch .tmpoutput
 status=0
+make uninstall >> .tmpoutput 2>&1 || status=1
 git pull >> .tmpoutput 2>&1 || status=1
 make install >> .tmpoutput 2>&1 || status=1
 printf "$status\n"
