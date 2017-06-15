3371f85b1a5071167bd313437248f7cfdba082b0
hn1815 <harry.nash15@imperial.ac.uk>
2017-06-15 15:12:41 +0100

    Added extra missing props to construtor

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -179,7 +179,7 @@ function ImgSquare(props) {
 */
 
 class ImgSquare extends Component {
-  constructor() {
+  constructor(props) {
     super(props);
     this.handleMouseOver = this.handleMouseOver.bind(this);
     this.handleMouseOut = this.handleMouseOut.bind(this);
