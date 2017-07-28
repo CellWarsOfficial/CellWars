6687c27f8e05739d2424c8a8bcb1d793d42abc39
iarba <dociarba@gmail.com>
2017-07-28 14:43:04 +0100

    got database to work

--- database/index.js
+++ database/index.js
@@ -4,10 +4,32 @@ const sequelize = require("sequelize");
 const morgan = require("morgan");
 const cfgLoader = require("config.json");
 
-const cfg = cfgLoader('./config.json')
+const cfg = cfgLoader("./config.json")
 const svPort = process.env.PORT || cfg.svDefPort;
+const dbName = cfg.dbName;
+const dbUser = cfg.dbUser;
+const dbPass = cfg.dbPass;
+const dbDialect = cfg.dbType;
 
 const server = express();
+const db = new sequelize(
+  dbName,
+  dbUser,
+  dbPass,{
+  dialect: dbDialect
+});
+
+const Cell = db.define("cell", {
+  x:{
+    type: sequelize.NUMERIC
+  },
+  y:{
+    type: sequelize.NUMERIC
+  },
+  type:{
+    type: sequelize.NUMERIC
+  }
+});
 
 function sanitize(query){
   let obj = {};
@@ -31,23 +53,59 @@ function sanitize(query){
   } else {
     obj.py2 = query.py2;
   }
+  if(obj.px1 > obj.px2){
+    var aux = obj.px1;
+    obj.px1 = obj.px2;
+    obj.px2 = aux;
+  }
+  if(obj.py1 > obj.py2){
+    var aux = obj.py1;
+    obj.py1 = obj.py2;
+    obj.py2 = aux;
+  }
   return obj;
 }
 
 function init(){
+  db.authenticate()
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  Cell.sync({force: true}).then (() =>{
+    console.log("created table");
+  });
+
   server
     .use(cors())
-    .use(morgan('dev'))
+    .use(morgan("dev"))
     .get("/cells", (req, res, next) => {
       try{
         let details = sanitize(req.query);
-        res.status(200).send("Not implemented just yet");
+        Cell.findAll({ 
+          where: {
+            x: {
+              $gte: details.px1,
+              $lte: details.px2
+            },
+            y: {
+              $gte: details.py1,
+              $lte: details.py2
+            }
+          } 
+        })
+        .then((cellData) => {
+          res.status(200).send(cellData);
+        });
       } catch (err) {
         res.status(400).send(err);
       }
     })
     .listen(svPort, function () {
-      console.log('Example app listening on port ' + svPort.toString() + '!');
+      console.log("Example app listening on port " + svPort.toString() + "!");
     });
 }
 
