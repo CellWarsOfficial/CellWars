0f4e2e22bb0a3ad65a861937ac96b28b6b7be20f
iarba <dociarba@gmail.com>
2017-08-01 15:56:47 +0300

    updated deployment some more

--- run_up.sh
+++ run_up.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-rm -rf .tmpoutput
-touch .tmpoutput
-echo -e "HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n" >> .tmpoutput
+echo -e "HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n"
 status=0
-make uninstall >> .tmpoutput 2>&1 || status=1
-git pull >> .tmpoutput 2>&1 || status=1
-make install >> .tmpoutput 2>&1 || status=1
+make uninstall || status=1
+git pull || status=1
+make install || status=1
 printf "$status\n"
-cat .tmpoutput
 exit $status
