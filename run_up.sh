40b1e956c871ee686062eba1251a9f9a43d86c2c
iarba <dociarba@gmail.com>
2017-08-01 15:50:53 +0300

    updated deployment

--- run_up.sh
+++ run_up.sh
@@ -2,6 +2,7 @@
 
 rm -rf .tmpoutput
 touch .tmpoutput
+echo -e "HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n" >> .tmpoutput
 status=0
 make uninstall >> .tmpoutput 2>&1 || status=1
 git pull >> .tmpoutput 2>&1 || status=1
