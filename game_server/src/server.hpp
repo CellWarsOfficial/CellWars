aa10dbb476285c7b89662a3f4cd1a6bca7ae1614
iarba <dociarba@gmail.com>
2017-09-13 11:39:14 +0100

    refactored server.cpp into oblivion

--- game_server/src/server.hpp
+++ game_server/src/server.hpp
@@ -9,27 +9,11 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <string>
-#include <mutex>
-#include <map>
-#include <set>
 #include <constants.hpp>
 
-class Game; class Server;
+using namespace std;
 
-class WS_info
-{
-  public:
-  WS_info(string this_con, int id, int socketid);
-  void drop(Server *server);
-  std::mutex write_lock;
-  int task;
-  string this_con;
-  int id;
-  int s;
-  CELL_TYPE agent;
-  int ms;
-  std::set<int> expectation;
-};
+class Game;
 
 class Server
 {
@@ -37,42 +21,20 @@ class Server
   Server(int port, DB_conn *db, Logger *l);
   ~Server();
   void start(Game *game);
-  void bcast_message(string message);
-  void check_clients(uint8_t opcode);
-  bool taken(CELL_TYPE t);
-  void erase(int id);
   void demand_stat();
-  void inform(int task, int value);
-  int get_score(CELL_TYPE t);
   private:
   void act(int s, int id);
-  void hijack_ws(string this_con, int s, char *comm_buf);
-  string get_ws_msg(WS_info *w, char *comm_buf);
-  int handle_ws_write(WS_info *w, char *comm_buf, uint8_t opcode, string to_send);
-  void broadcaster(WS_info *w, uint8_t opcode, string to_send);
-  void forget(int id);
-  int serve_query(WS_info *w, string taskid, const char *point, char *comm_buf);
-  int serve_update(WS_info *w, string taskid, const char *point, char *comm_buf);
-  int serve_score(WS_info *w, string taskid, const char *point, char *comm_buf);
-  int serve_pick(WS_info *w, string taskid, const char *point, char *comm_buf);
-  int serve_details(WS_info *w, string taskid, const char *point, char *comm_buf);
-  int serve_capitals(WS_info *w, string taskid, const char *point, char *comm_buf);
   int socketid;
   DB_conn *db_info;
   Game *game;
   Logger *log;
-  int live_conns;
-  int next_ws;
-  int conns;
-  std::mutex server_lock;
-  std::map <int, WS_info *> monitor;
 };
 
-FILE *get_404();
+int create_server_socket(int port);
 
-FILE *get_file(string file);
+int create_connection_socket(const char *remote, int port);
 
-void catfile(FILE *f, int s, char *buf);
+int create_connection_socket(string remote, int port);
 
 int deny_access(int s);
 
