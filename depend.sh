e9414ece2388386325ead775ba4af05bc9499ed0
Paul Crestez <paul.crestez15@imperial.ac.uk>
2017-10-13 21:17:14 +0100

    further cleanup within root repository, fixed oversight with updater

--- depend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "installing dependencies"
-cd react/my-app
-npm install
-cd ../..
--- run_up.sh
+++ run_up.sh
@@ -5,5 +5,5 @@ status=0
 make uninstall || status=1
 git pull || status=1
 make install || status=1
-printf "$status\n"
+printf "$status\r\n"
 exit $status
